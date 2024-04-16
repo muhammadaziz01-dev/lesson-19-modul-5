e710cb1dcfd39111077be68abe3b06a1ec442b37
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-04-16 06:10:49 +0300

    add

--- src/layout/erorr-layout/index.jsx
+++ src/layout/erorr-layout/index.jsx
@@ -1,26 +1,27 @@
+import { Link } from "react-router-dom";
 import "./style.scss"
 
 function index() {
     return <>
-        <section class="page_404">
-            <div class="container mx-auto px-[20px]">
-                <div class="row">
-                    <div class="col-sm-12 ">
-                        <div class="col-sm-10 col-sm-offset-1  text-center">
-                            <div class="four_zero_four_bg">
-                                <h1 class="text-center ">404</h1>
+        <section className="page_404">
+            <div className="container mx-auto px-[">
+                <div className="row">
+                    <div className="col-sm-12 ">
+                        <div className="col-sm-10 col-sm-offset-1  text-center">
+                            <div className="four_zero_four_bg">
+                                <h1 className="text-center ">404</h1>
 
 
                             </div>
 
-                            <div class="contant_box_404">
-                                <h3 class="h2">
+                            <div className="contant_box_404">
+                                <h3 className="h2">
                                     Look like you're lost
                                 </h3>
 
                                 <p>the page you are looking for not avaible!</p>
 
-                                <a href="" class="link_404">Go to Home</a>
+                                <Link to="/" className="link_404">Go to Home</Link>
                             </div>
                         </div>
                     </div>
